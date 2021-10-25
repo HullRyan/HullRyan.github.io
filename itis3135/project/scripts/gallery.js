4b054a8444054206145f16158cd6138f53b573a9
Ryan Hull <hull.ryanc@gmail.com>
2021-10-25 16:38:51 -0400

    Project testing

---
+++ itis3135/project/scripts/gallery.js
@@ -0,0 +1,37 @@
+let headersList = {
+  Accept: "*/*",
+  "User-Agent": "Thunder Client (https://www.thunderclient.io)",
+};
+
+function getPhotos() {
+  $.getJSON(
+    "https://api.flickr.com/services/rest?api_key=151dd3c0755a192df5e460420b7a8521&method=flickr.photosets.getPhotos&photoset_id=72157720050478418&format=json&jsoncallback=?",
+    {}
+  ).done(function (data) {
+    console.log(data);
+    var div = document.querySelector(".images");
+
+    $.each(data.photoset.photo, function (i, gp) {
+      var farmId = gp.farm;
+      var serverId = gp.server;
+      var id = gp.id;
+      var secret = gp.secret;
+
+      console.log(farmId + ", " + serverId + ", " + id + ", " + secret);
+
+      //  https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
+
+      $(".images").append(
+        '<img src="https://farm' +
+          farmId +
+          ".staticflickr.com/" +
+          serverId +
+          "/" +
+          id +
+          "_" +
+          secret +
+          '.jpg"/>'
+      );
+    });
+  });
+}
